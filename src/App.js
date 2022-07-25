9d65bdcf6c4806f5413ad924015f1909ceacaeac
miriacode <miriamacunaenciso@gmail.com>
2022-07-24 23:15:24 -0500

    Adding  routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css';
+import { Layout } from "./Layout"
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={}></Route>
+          <Route path="/" element={<Layout/>}>
+            <Route index element={<Home/>}/>
+            <Route path="/search/:query" element={<Results/>}/>
+            <Route path="/news/:id" element={<NewsDetail/>}/>
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
